f5dec1ffd103c4507352028e06c6897e2267b737
JOHAN BLANCO <johansneyder.bs19@gmail.com>
2024-10-02 15:27:19 -0400

    cambios

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
+        // johanks19
+        // johan Sneyder Blanco Sanchez
+
 
     }
 }
