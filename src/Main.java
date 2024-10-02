dd592f930bc691cf167fa656108029749c263417
Your Name <you@example.com>
2024-10-02 15:20:23 -0500

    samuel1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
+        // samuelolarte2802
+        // Samuel David OLarte Tenjo
 
     }
 }
