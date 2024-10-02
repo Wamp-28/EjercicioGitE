1b230f3e364853f0553f44bfae19f61de77b2ae9
Santiago Pinzon <santiagpinzon@gmail.com>
2024-10-02 15:19:55 -0500

    cambio 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
+        // SantiagT1
+        // Santiago Pinzon Tenorio
 
     }
 }
