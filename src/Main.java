34bfb7f1265634afe32c6294e0af069d2ba64bdb
elianiss <elianaguevara0203@gmail.com>
2024-10-02 15:24:45 -0500

    datos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
+        // USUARIO GIT : elianiss
+        // NOMBRE COMPLETO : Eliana Camila Guevara Cubillos
 
     }
 }
