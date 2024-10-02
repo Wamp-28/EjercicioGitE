e7582ddd50878bcc92633410d5e967bf15b9a027
brayan7122 <brayanrativa712@gmail.com>
2024-10-02 15:22:59 -0500

    Datos de usuario

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
-
+        // brayan7122
+        // Brayan Smith Matabanchoy Zambrano
     }
 }
