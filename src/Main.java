c1cbe1ea2226ff57808e80ede5a3c0e3af75b204
zip-b205-e-018 <andresfespitia2001@gmail.com>
2024-10-02 15:21:32 -0500

    perros Femenino

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
+        // miguelon02
+        // miguel angel rojas rodriguez
 
+        Map<String, Perras> perros = new HashMap<>();
+        perros.put("1",new Perras());
+
+        for (String clave: perros.keySet()){
+            System.out.println("clave: "+clave+", info: "+perros.get(clave));
+        }
+        perros.remove("1");
     }
 }
