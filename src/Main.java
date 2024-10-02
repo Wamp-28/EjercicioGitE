a7222d7ab46ddf034ab88497c5928a42c5607121
Brayan Arturo <brayanarturotorresilva21@gmail.com>
2024-10-02 15:21:44 -0500

    Brayan1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
+        // Brayan-ai-jpg-bit
+        // Brayan Arturo Torres Silva
 
     }
 }
