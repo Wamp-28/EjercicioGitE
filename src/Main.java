82a98fad8105c6c769ce52772c6316395d78ffe0
Juan Rozo <jucarope2006@gmail.com>
2024-10-02 15:51:59 -0500

    datos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         // USUARIO GIT
+        // JuanRozo18
+
         // NOMBRE COMPLETO
+        // Juan Camilo Rozo Penagos
 
     }
 }
