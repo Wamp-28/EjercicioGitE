2940aebf1d081aa97b6286cb9cda94ca27e70615
NMirandaMolina <naomyranda04@gmail.com>
2024-10-02 15:28:06 -0500

    nao1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         // NMirandaMolina
-        // Karen Naomy Miranda Molina
+        // Karen Naomy Miranda Molina.
 
     }
 }
