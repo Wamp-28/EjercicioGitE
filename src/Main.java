c546f91fda1a9eedfe3de00f6eb808e0ea9731f5
Xbraxter <daniel.barrera-se@uniminuto.edu.co>
2024-10-02 15:21:26 -0500

    datos usuario

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // USUARIO GIT
-        // NOMBRE COMPLETO
+        // Usuario: Xbraster
+        // NOMBRE COMPLETO: Daniel Felipe Barrera Segura
 
     }
 }
